117d374985a230f5b23a4ba948766505a78cb0bf
flow <senb.zhuang@gmail.com>
2021-09-21 15:16:48 +0800

    离开上报优化;

--- core/index.ts
+++ core/index.ts
@@ -1,4 +1,4 @@
-import { initLifecycle } from './lifecycle'
+import { initLifecycle, initEvent } from './lifecycle'
 import { initErrorCollect } from './error'
 import { initHistoryCollect } from './history'
 import { initPerformanceCollect } from './performance'
@@ -8,4 +8,6 @@ export function init(monitor) {
   initErrorCollect(monitor)
   initHistoryCollect(monitor)
   initPerformanceCollect(monitor)
+  // 最后监听事件, 确保monitor的beforeunload上报最后执行
+  initEvent(monitor)
 }
--- core/lifecycle.ts
+++ core/lifecycle.ts
@@ -1,6 +1,6 @@
 import { fill, eventTrigger, on } from '../utils'
 import { Monitor } from '../types'
-import { CACHE_KEY } from '../shared/constants'
+// import { CACHE_KEY } from '../shared/constants'
 
 function wrapXMLHttpRequest(monitor: Monitor) {
   const url = monitor.options.url
@@ -49,47 +49,52 @@ function wrapFetch(monitor: Monitor) {
   })
 }
 
-// function send(url: string, data: any[]) {
-//   if (navigator.sendBeacon) {
-//     navigator.sendBeacon(url, JSON.stringify(data))
-//   } else {
-//     const xhr = new XMLHttpRequest()
-//     xhr.open('POST', url, false) // 同步
-//     // 需在 open 和 send 之间调用
-//     xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-//     xhr.send(JSON.stringify(data))
-//   }
-// }
+function send(url: string, data: any[]) {
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, JSON.stringify(data))
+  } else {
+    // 也可用new Image
+    const xhr = new XMLHttpRequest()
+    xhr.open('POST', url, false) // 同步
+    // 需在 open 和 send 之间调用
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    xhr.send(JSON.stringify(data))
+  }
+}
 
-// let alreadySent = false
+let alreadySent = false
 
 function initUnloadListener(monitor: Monitor) {
-  // function unloadHandler() {
-  //   if (alreadySent) return
-  //   alreadySent = true
-  //   const { options, $tracker } = monitor
-  //   const data = $tracker.getNotSentData()
-  //   if (data && data.length > 0) {
-  //     send(options.url, data)
-  //   }
-  // }
+  function unloadHandler() {
+    if (alreadySent) return
+    alreadySent = true
+    const { options, $tracker } = monitor
+    const data = $tracker.getNotSentData()
+    if (data && data.length > 0) {
+      send(options.url, data)
+    }
+  }
 
-  // on(window, 'unload', unloadHandler)
+  on(window, 'unload', unloadHandler)
+  on(window, 'beforeunload', unloadHandler)
 
   // 缓存未发送数据, 考虑打开多tab
-  on(window, 'unload', function () {
-    const { $tracker } = monitor
-    const data = $tracker.getNotSentData()
-    let cacheData = localStorage.getItem(CACHE_KEY)
-    cacheData = (cacheData && JSON.parse(cacheData)) || []
-    // @ts-ignore
-    cacheData = cacheData.concat(data)
-    localStorage.setItem(CACHE_KEY, JSON.stringify(cacheData))
-  })
+  // on(window, 'unload', function () {
+  //   const { $tracker } = monitor
+  //   const data = $tracker.getNotSentData()
+  //   let cacheData = localStorage.getItem(CACHE_KEY)
+  //   cacheData = (cacheData && JSON.parse(cacheData)) || []
+  //   // @ts-ignore
+  //   cacheData = cacheData.concat(data)
+  //   localStorage.setItem(CACHE_KEY, JSON.stringify(cacheData))
+  // })
 }
 
 export function initLifecycle(monitor) {
   wrapXMLHttpRequest(monitor)
   wrapFetch(monitor)
+}
+
+export function initEvent(monitor) {
   initUnloadListener(monitor)
 }
